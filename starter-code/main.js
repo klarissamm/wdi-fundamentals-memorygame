5096a9618deab5b8dd527aef18da7e0cca26fa47
Klarissa Munz <klarissamunz@Klarissas-MacBook-Pro.local>
2016-11-15 11:02:26 -0800

    created comparing statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,13 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo || cardThree === cardFour) {
+  window.alert("You found a match!");
+} else if (cardTwo === cardFour) {
+	window.alert("You found a match!");
+} else {
+  window.alert("Sorry, try again.");
+}
+
+
