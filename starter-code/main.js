909b2df174dd4ad1b965b89927b142f9a6c04f19
Klarissa Munz <klarissamunz@Klarissas-MacBook-Pro.local>
2016-11-17 09:00:48 -0800

    created cards with javaScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,16 +1,31 @@
 console.log("JS file is connected to HTML! Woo!")
 
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo || cardThree === cardFour) {
+
+/*if (cardOne === cardTwo || cardThree === cardFour) {
   window.alert("You found a match!");
 } else if (cardTwo === cardFour) {
 	window.alert("You found a match!");
 } else {
   window.alert("Sorry, try again.");
-}
+}*/
+
+var board = document.getElementById('game-board');
+console.log(board);
+var createCards = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    board.appendChild(cardElement);
+  }
+};
+createCards();
 
 
