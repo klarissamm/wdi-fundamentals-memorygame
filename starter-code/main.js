05519522389daddbfe4cb9a92f7ee5b8649c6791
Klarissa Munz <klarissamunz@Klarissas-MacBook-Pro.local>
2016-11-18 11:05:40 -0800

    created working memory game and added images

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,30 +2,55 @@ console.log("JS file is connected to HTML! Woo!")
 
 
 var cards = ['queen', 'queen', 'king', 'king'];
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-
-/*if (cardOne === cardTwo || cardThree === cardFour) {
-  window.alert("You found a match!");
-} else if (cardTwo === cardFour) {
-	window.alert("You found a match!");
-} else {
-  window.alert("Sorry, try again.");
-}*/
+var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
-console.log(board);
-var createCards = function() {
+
+function createBoard() {
   for (var i = 0; i < cards.length; i++) {
     var cardElement = document.createElement('div');
     cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    board.appendChild(cardElement);
     board.appendChild(cardElement);
   }
-};
-createCards();
+  console.log(board);
+}
+
+function isTwoCards () {
+	cardsInPlay.push(this.getAttribute('data-card'));
+		console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = '<img src="king.png" alt="King of Hearts">';
+	} else {
+		this.innerHTML = '<img src="queen.png" alt="Queen of Spades">';
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+  	window.alert("You found a match!");
+} else {
+  window.alert("Sorry, try again.");
+}
+}
+
+
+createBoard();
+
+/*
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";*/
+
+
+
 
 
